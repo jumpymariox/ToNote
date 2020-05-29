65c3c4075877aee70000996b8ddd0919c1c04c5f
yaozhangpeng <dreamgod@outlook.com>
2020-05-29 11:41:10 +0800

    add tarbar

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -4,7 +4,12 @@
 const app = getApp<IAppOption>()
 
 Page({
-  data: {},
+  data: {
+    tabbar: [
+      { text: "笔记", iconPath: "../../../asset/img/note.png", selectedIconPath: "../../../asset/img/note_active.png" },
+      { text: "个人", iconPath: "../../../asset/img/user.png", selectedIconPath: "../../../asset/img/user_active.png" }
+    ]
+  },
   onLoad() {
   }
 })
--- miniprogram/service/note.service.ts
+++ miniprogram/service/note.service.ts
@@ -6,7 +6,7 @@ export class NoteService {
   fetchNotes(): Promise<{ notes: Note[] }> {
     return new Promise((resolve: (value: { notes: Note[] }) => void) => {
       wx.request({
-        url: "http://localhost:3000/notes", success() {
+        url: "http://192.168.88.106:3000/notes", success() {
           resolve({ notes: MOCK.notes });
         }, fail() {
 
